42ec4c8fe85761f716bba30950dc9317cc700579
if23b143 <162723934+if23b143@users.noreply.github.com>
2024-03-16 14:36:58 +0100

    Testing the switch casing in python

---
+++ hashtabelle.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+numberinput = int(input("Geben Sie eine Zahl ein: "))
+
+match numberinput:
+    case 1:
+        print("1")
+    case 2:
+        print("2")
+    case 3:
+        print("3")
+    case 4:
+        print("4")
+    case 5:
+        print("5")
+    case 6:
+        print("6")
+    case 7:
+        print("7")
+    case default:
+        print("default")
+
