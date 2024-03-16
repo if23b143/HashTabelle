933125bc7c8c6e15c985e00d5bad631a7b15ae52
KarlosSCP <162018366+KarlosSCP@users.noreply.github.com>
2024-03-16 14:39:07 +0100

    Update hashtabelle.py

--- hashtabelle.py
+++ hashtabelle.py
@@ -8,19 +8,19 @@ numberinput = int(input("Geben Sie eine Zahl ein: "))
 
 match numberinput:
     case 1:
-        print("1")
+        print("1. ADD")
     case 2:
-        print("2")
+        print("2. DEL")
     case 3:
-        print("3")
+        print("3. IMPORT")
     case 4:
-        print("4")
+        print("4. SEARCH")
     case 5:
-        print("5")
+        print("5. PLOT")
     case 6:
-        print("6")
+        print("6. SAVE")
     case 7:
-        print("7")
+        print("7. LOAD")
     case default:
         print("default")
 
